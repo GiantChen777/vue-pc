a5a6baccc7fce3dff3aedff294aae186cdad35f6
chenchenchen <2833643915@qq.com>
2020-11-27 19:43:49 +0800

    feat:Header组件完成

--- src/components/Headers/index.vue
+++ src/components/Headers/index.vue
@@ -1,14 +1,145 @@
 <template>
-  <div>Header...</div>
+  <div class="header-container">
+    <div class="header">
+      <div class="header-top">
+        <div class="header-login-list">
+          <p>尚品汇欢迎您！</p>
+          <span>请</span>
+          <router-link to="/login">登录</router-link>
+          <router-link to="/register" class="register">免费注册</router-link>
+        </div>
+        <div class="header-type-list">
+          <ul>
+            <li>
+              <a href="###" class="first">我的订单</a>
+            </li>
+            <li>
+              <a href="###">我的购物车</a>
+            </li>
+            <li>
+              <a href="###">我的尚品汇</a>
+            </li>
+            <li>
+              <a href="###">尚品汇会员</a>
+            </li>
+            <li>
+              <a href="###">企业采购</a>
+            </li>
+            <li>
+              <a href="###">关注尚品汇</a>
+            </li>
+            <li>
+              <a href="###">我的订单</a>
+            </li>
+            <li>
+              <a href="###">合作招商</a>
+            </li>
+            <li>
+              <a href="###">商家后台</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="header-bottom">
+      <div class="header-img">
+        <router-link to="/">
+          <img src="./images/logo.png" />
+        </router-link>
+      </div>
+      <div class="header-input">
+        <input type="text" class="header-text" />
+        <button @click="search" class="header-button" type="button">
+          搜索
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Header',
-  data() {
-    return {}
+  methods: {
+    search() {
+      this.$router.push('/search')
+    },
   },
 }
 </script>
-<style scoped>
+<style lang="less" scoped>
+.header {
+  width: 100%;
+  height: 30px;
+  background-color: #eaeaea;
+}
+.header-top {
+  width: 1200px;
+  height: 30px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+.header-top a:hover {
+  color: pink;
+  text-decoration: none;
+}
+
+.header-login-list {
+  display: flex;
+  align-items: center;
+}
+.header-login-list p {
+  margin: 0 10px 0 0;
+}
+.header-login-list a.register {
+  margin: 0 0 0 5px;
+  padding: 0 0 0 5px;
+  border-left: 1px solid #ccc;
+}
+.header-type-list ul {
+  height: 30px;
+  line-height: 30px;
+  display: flex;
+}
+.header-type-list ul li a {
+  margin: 0 5px;
+  padding: 0 0 0 5px;
+  border-left: 1px solid #ccc;
+}
+.header-type-list ul li a.first {
+  border-left: none;
+}
+.header-bottom {
+  width: 1200px;
+  height: 67px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 0 11px 0;
+}
+.header-bottom .header-img {
+  width: 176px;
+  height: 56px;
+}
+.header-bottom .header-input {
+  display: flex;
+  align-items: center;
+}
+.header-input .header-text {
+  width: 490px;
+  height: 32px;
+  border: 2px solid #ea4a36;
+  outline: none;
+  box-sizing: border-box;
+}
+.header-input .header-button {
+  width: 68px;
+  height: 32px;
+  background-color: #ea4a36;
+  color: #fff;
+  outline: none;
+  border: none;
+  cursor: pointer;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App'
 
 import router from "./router"
+// 引入公共资源样式
+import "./style/reset.css"
 
 Vue.config.productionTip = false
 
